4e2e0c8794cf622fab3caba87837a0b41cdc1946
euglazer <eugenestapler@gmail.com>
2015-04-27 11:34:28 +1200

    modify group serializer to only return pending rounds if the current_user is an admin of the rounds' group

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,4 +1,9 @@
 class GroupSerializer < ActiveModel::Serializer
   attributes :id, :name
   has_many :rounds, serializer: RoundShortSerializer
+
+  def rounds
+    object.rounds.select { |round| round.mode != "pending" || round.is_admin?(current_user) }
+  end
+
 end
